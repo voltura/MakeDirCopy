a58b106b7983d70fe6e0e02ddfc9a5e505d0f5e1
Joakim Skoglund <joakim@voltura.se>
2022-01-18 05:21:23 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace MakeDirCopy
+{
+    class Program
+    {
+        static void Main()
+        {
+            ProcessFiles();
+        }
+
+        private static void ProcessFiles()
+        {
+            foreach (FileInfo fi in GetFileInfosFromDir(AppDomain.CurrentDomain.BaseDirectory)) if (FileToBeProcessed(fi)) ProcessFile(fi);
+        }
+
+        private static void ProcessFile(FileInfo fi)
+        {
+            var d = GetNewDirName(fi);
+            CreateDir(d);
+            MoveFile(fi, d);
+        }
+
+        private static string GetNewDirName(FileInfo fi)
+        {
+            return Path.Combine(fi.DirectoryName, Path.GetFileNameWithoutExtension(fi.Name));
+        }
+
+        private static void MoveFile(FileInfo fi, string dir)
+        {
+            Console.WriteLine($"Processing {fi.Name}...");
+            File.Move(fi.FullName, Path.Combine(dir, fi.Name));
+        }
+
+        private static void CreateDir(string dir)
+        {
+            Directory.CreateDirectory(dir);
+        }
+
+        private static bool FileToBeProcessed(FileInfo fi)
+        {
+            return !(fi.Attributes.HasFlag(FileAttributes.Directory) || fi.Extension.Contains("qt") || fi.Extension.Contains("exe"));
+        }
+
+        private static FileInfo[] GetFileInfosFromDir(string path)
+        {
+            return new DirectoryInfo(path).GetFiles("*.*", SearchOption.TopDirectoryOnly);
+        }
+    }
+}
