2b8182adb5e8ba185905d1297bf024114de9776d
Joakim Skoglund <joakim@voltura.se>
2022-06-03 00:58:26 +0200

    refactoring

--- Program.cs
+++ Program.cs
@@ -5,10 +5,7 @@ namespace MakeDirCopy
 {
     class Program
     {
-        static void Main()
-        {
-            ProcessFiles();
-        }
+        static void Main() => ProcessFiles();
 
         private static void ProcessFiles()
         {
@@ -22,30 +19,14 @@ namespace MakeDirCopy
             MoveFile(fi, d);
         }
 
-        private static string GetNewDirName(FileInfo fi)
-        {
-            return Path.Combine(fi.DirectoryName, Path.GetFileNameWithoutExtension(fi.Name));
-        }
+        private static string GetNewDirName(FileInfo fi) => Path.Combine(fi.DirectoryName, Path.GetFileNameWithoutExtension(fi.Name));
 
-        private static void MoveFile(FileInfo fi, string dir)
-        {
-            Console.WriteLine($"Processing {fi.Name}...");
-            File.Move(fi.FullName, Path.Combine(dir, fi.Name));
-        }
+        private static void MoveFile(FileInfo fi, string dir) => File.Move(fi.FullName, Path.Combine(dir, fi.Name));
 
-        private static void CreateDir(string dir)
-        {
-            Directory.CreateDirectory(dir);
-        }
+        private static void CreateDir(string dir) => Directory.CreateDirectory(dir);
 
-        private static bool FileToBeProcessed(FileInfo fi)
-        {
-            return !(fi.Attributes.HasFlag(FileAttributes.Directory) || fi.Extension.Contains("qt") || fi.Extension.Contains("exe"));
-        }
+        private static bool FileToBeProcessed(FileInfo fi) => !(fi.Attributes.HasFlag(FileAttributes.Directory) || fi.Extension.Contains("qt") || fi.Extension.Contains("exe"));
 
-        private static FileInfo[] GetFileInfosFromDir(string path)
-        {
-            return new DirectoryInfo(path).GetFiles("*.*", SearchOption.TopDirectoryOnly);
-        }
+        private static FileInfo[] GetFileInfosFromDir(string path) => new DirectoryInfo(path).GetFiles("*.*", SearchOption.TopDirectoryOnly);
     }
 }
