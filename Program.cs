0f710bb5b58b96a2b734e3ba06bfe7ffa367ccb0
Joakim Skoglund <joakim@voltura.se>
2022-06-03 01:57:51 +0200

    Changed output to AOT compiled exe from .NET 6
    Transformation

--- Program.cs
+++ Program.cs
@@ -1,32 +1,23 @@
-﻿using System;
-using System.IO;
+﻿ProcessFiles();
 
-namespace MakeDirCopy
+static void ProcessFiles()
 {
-    class Program
-    {
-        static void Main() => ProcessFiles();
-
-        private static void ProcessFiles()
-        {
-            foreach (FileInfo fi in GetFileInfosFromDir(AppDomain.CurrentDomain.BaseDirectory)) if (FileToBeProcessed(fi)) ProcessFile(fi);
-        }
-
-        private static void ProcessFile(FileInfo fi)
-        {
-            var d = GetNewDirName(fi);
-            CreateDir(d);
-            MoveFile(fi, d);
-        }
-
-        private static string GetNewDirName(FileInfo fi) => Path.Combine(fi.DirectoryName, Path.GetFileNameWithoutExtension(fi.Name));
-
-        private static void MoveFile(FileInfo fi, string dir) => File.Move(fi.FullName, Path.Combine(dir, fi.Name));
-
-        private static void CreateDir(string dir) => Directory.CreateDirectory(dir);
-
-        private static bool FileToBeProcessed(FileInfo fi) => !(fi.Attributes.HasFlag(FileAttributes.Directory) || fi.Extension.Contains("qt") || fi.Extension.Contains("exe"));
-
-        private static FileInfo[] GetFileInfosFromDir(string path) => new DirectoryInfo(path).GetFiles("*.*", SearchOption.TopDirectoryOnly);
-    }
+    foreach (FileInfo fi in GetFileInfosFromDir(AppDomain.CurrentDomain.BaseDirectory)) if (FileToBeProcessed(fi)) ProcessFile(fi);
 }
+
+static void ProcessFile(FileInfo fi)
+{
+    var d = GetNewDirName(fi);
+    CreateDir(d);
+    MoveFile(fi, d);
+}
+
+static string GetNewDirName(FileInfo fi) => Path.Combine(fi.DirectoryName is null ? string.Empty : fi.DirectoryName, Path.GetFileNameWithoutExtension(fi.Name));
+
+static void MoveFile(FileInfo fi, string dir) => File.Move(fi.FullName, Path.Combine(dir, fi.Name));
+
+static void CreateDir(string dir) => Directory.CreateDirectory(dir);
+
+static bool FileToBeProcessed(FileInfo fi) => !(fi.Attributes.HasFlag(FileAttributes.Directory) || fi.Extension.Contains("qt") || fi.Extension.Contains("exe"));
+
+static FileInfo[] GetFileInfosFromDir(string path) => new DirectoryInfo(path).GetFiles("*.*", SearchOption.TopDirectoryOnly);
